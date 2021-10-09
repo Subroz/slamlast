7c4fb1df2192fcece12152f4f0e17f4b030008fe
Subro <57826642+Subroz@users.noreply.github.com>
2021-10-10 00:59:41 +0600

    Update qbit_downloader.py

--- bot/helper/mirror_utils/download_utils/qbit_downloader.py
+++ bot/helper/mirror_utils/download_utils/qbit_downloader.py
@@ -171,7 +171,7 @@ class QbitTorrent:
                         self.client.auth_log_out()
                         self.updater.cancel()
             elif tor_info.state == "stalledDL":
-                if time.time() - self.stalled_time >= 999999999: # timeout after downloading metadata
+                if time.time() - self.stalled_time >= 9999999: # timeout after downloading metadata
                     self.client.torrents_pause(torrent_hashes=self.ext_hash)
                     time.sleep(0.3)
                     self.listener.onDownloadError("Dead Torrent!")
